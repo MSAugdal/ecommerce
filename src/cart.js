f9de657e0e2be55f4d09a82c4b322af4cb27a480
MSAugdal <mathias.s.augdal@gmail.com>
2023-04-12 20:17:02 +0200

    fixed JS

--- src/cart.js
+++ src/cart.js
@@ -4,37 +4,37 @@ let shopItemsData = [
 		id: "iphone_11",
 		name: "iPhone 11",
 		price: 1000,
-		img: "../images/iphone_11.png",
+		img: "images/iphone_11.png",
 	},
 	{
 		id: "iphone_11Pro",
 		name: "iPhone 11 Pro",
 		price: 1200,
-		img: "../images/iphone_11Pro.png",
+		img: "images/iphone_11Pro.png",
 	},
 	{
 		id: "iphone_11ProMax",
 		name: "iPhone 11 Pro Max",
 		price: 1400,
-		img: "../images/iphone_11ProMax.png",
+		img: "images/iphone_11ProMax.png",
 	},
 	{
 		id: "iphone_12",
 		name: "iPhone 12",
 		price: 1600,
-		img: "../images/iphone_12.png",
+		img: "images/iphone_12.png",
 	},
 	{
 		id: "iphone_12Pro",
 		name: "iPhone 12 Pro",
 		price: 1800,
-		img: "../images/iphone_12Pro.png",
+		img: "images/iphone_12Pro.png",
 	},
 	{
 		id: "iphone_12ProMax",
 		name: "iPhone 12 Pro Max",
 		price: 2000,
-		img: "../images/iphone_12ProMax.png",
+		img: "images/iphone_12ProMax.png",
 	},
 ];
 
